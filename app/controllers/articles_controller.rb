a0b6fe1d870436bcc9806f700f0873053298f86a
Dalal Alrayes <dalrayes@gmail.com>
2015-02-02 18:51:37 -0600

    CRUD actions working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,4 +11,37 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
+
+	def create
+		@article = Article.new(article_params)
+		if @article.valid?
+			@article.save
+		end
+		redirect_to articles_path
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+		edit_article_path(@article)
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update_attributes(article_params)
+		redirect_to articles_path
+	end
+
+	def destroy
+		@article =Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
+	end
+
+	private
+
+	def article_params
+		params.require(:article).permit(:title, :body)
+	end
+
+
 end
